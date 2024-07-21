7c354406f602e49ecc1f15fffaeb124ae224ea76
Phurin Nararat <phurin.nrt@gmail.com>
2024-07-21 23:56:16 +0700

    Delete mqtt.py
    Chore: remove unused "mqtt.py" it is no need for this project

--- mqtt.py
+++
@@ -1,9 +0,0 @@
-import paho.mqtt.publish as pub, paho.mqtt.subscribe as sub, json
-
-def publish(topic:str, payload:dict, hostname:str):
-    pub.single(topic = topic, payload = json.dumps(payload), hostname = hostname)
-
-def subscribe(topic:str, hostname:str):
-    msg = sub.simple(topics = topic, hostname = hostname)
-    data = json.loads(msg.payload.decode()) # type: ignore
-    return data
